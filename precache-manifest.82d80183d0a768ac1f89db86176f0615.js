c3e310938fd060b98be45e8b383d7bc023881f9e
Mikhail Shardin <kriptonit1988@gmail.com>
2019-08-10 11:27:57 +0500

    Updates

---
+++ precache-manifest.82d80183d0a768ac1f89db86176f0615.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f650ef0411b21b803cfee1fe82413f9",
+    "url": "/todo-list-with-hooks/index.html"
+  },
+  {
+    "revision": "10f598f2c0d4bea14e2c",
+    "url": "/todo-list-with-hooks/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e9eb2765800b3e8516d8",
+    "url": "/todo-list-with-hooks/static/js/2.5853b9f6.chunk.js"
+  },
+  {
+    "revision": "10f598f2c0d4bea14e2c",
+    "url": "/todo-list-with-hooks/static/js/main.e6a32389.chunk.js"
+  },
+  {
+    "revision": "a9a68064e5204c06e26b",
+    "url": "/todo-list-with-hooks/static/js/runtime~main.96b29ddb.js"
+  }
+]);
